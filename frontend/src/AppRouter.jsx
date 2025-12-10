3729b4a8db3ad9cfbfc3fe8fe89c83ec3a91943e
Yousef emad mikhail <mad33833@gmail.com>
2025-12-10 13:45:58 +0200

    fix issue in order details page

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,20 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./pages/MainLayout";
-import NotFound from './pages/NotFound';
-import Home from './pages/Home';
-import RecipeList from './pages/RecipeList';
-import RecipeDetails from './pages/RecipeDetails';
-import Market from './pages/Market';
-import IngredientDetails from './pages/IngredientDetails';
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import RecipeList from "./pages/RecipeList";
+import RecipeDetails from "./pages/RecipeDetails";
+import Market from "./pages/Market";
+import IngredientDetails from "./pages/IngredientDetails";
 import Checkout from "./pages/Checkout";
-import Categories from './pages/Categories';
-import Countries from './pages/Countries';
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
+import Categories from "./pages/Categories";
+import Countries from "./pages/Countries";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 import ForgetPassword from "./pages/Auth/ForgetPassword";
-import ResetPassword from './pages/Auth/ResetPassword';
-import VerifyEmail from './pages/Auth/VerifyEmail';
-import TwoFactorAuth from './pages/Auth/TwoFactorAuth';
+import ResetPassword from "./pages/Auth/ResetPassword";
+import VerifyEmail from "./pages/Auth/VerifyEmail";
+import TwoFactorAuth from "./pages/Auth/TwoFactorAuth";
 import AuthLayout from "./pages/Auth/AuthLayout";
 import Profile from "./pages/Profile";
 import Chat from "./pages/Chat";
@@ -30,47 +30,47 @@ import Orders from "./pages/Orders";
 import OrderDetails from "./pages/OrderDetails";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainLayout />,
-        errorElement: <NotFound />,
-        children: [
-            { index: true, element: <Home /> },
-            { path: "recipes", element: <RecipeList /> },
-            { path: "/recipe/:slug", element: <RecipeDetails /> },
-            { path: "market", element: <Market /> },
-            { path: "ingredient/:slug", element: <IngredientDetails /> },
-            { path: "checkout", element: <Checkout /> },
-            { path: "categories", element: <Categories /> },
-            { path: "countries", element: <Countries /> },
-            { path: "chat", element: <Chat/>},
-            { path: "cart", element: <Cart/> },
-            { path: "profile", element: <Profile/> },
-            { path: "diet-plan", element: <DietPlan/> },
-            { path: "search-ai/:query", element: <SearchWithAi/> },
-            {path: "recommendation/:name", element: <RecommendationDetails/>},
-            { path: "favourites", element: <Favorite/>},
-            { path: "contact", element: <Contact /> },
-            { path: "terms-and-conditions", element: <TermsAndConditions /> },
-            {path:"/order",element:<Order/>},
-            {path:"/orders",element:<Orders/>},
-            {path:"/order/:orderId",element:<OrderDetails/>},
-            { path: "*", element: <NotFound /> }
-        ],
-    },
-    {
-        path: "/auth",
-        element: <AuthLayout />,
-        children: [
-            { index: true, element: <Login /> },
-            { path: "login", element: <Login /> },
-            { path: "register", element: <Register /> },
-            { path: "forget-password", element: <ForgetPassword /> },
-            { path: "reset-password/:token", element: <ResetPassword /> },
-            { path: "verify-email/:token", element: <VerifyEmail /> },
-            { path: "2fa", element: <TwoFactorAuth /> },
-        ],
-    }
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "recipes", element: <RecipeList /> },
+      { path: "/recipe/:slug", element: <RecipeDetails /> },
+      { path: "market", element: <Market /> },
+      { path: "ingredient/:slug", element: <IngredientDetails /> },
+      { path: "checkout", element: <Checkout /> },
+      { path: "categories", element: <Categories /> },
+      { path: "countries", element: <Countries /> },
+      { path: "chat", element: <Chat /> },
+      { path: "cart", element: <Cart /> },
+      { path: "profile", element: <Profile /> },
+      { path: "diet-plan", element: <DietPlan /> },
+      { path: "search-ai/:query", element: <SearchWithAi /> },
+      { path: "recommendation/:name", element: <RecommendationDetails /> },
+      { path: "favourites", element: <Favorite /> },
+      { path: "contact", element: <Contact /> },
+      { path: "terms-and-conditions", element: <TermsAndConditions /> },
+      { path: "/orders", element: <Orders /> },
+      { path: "/order/:orderId", element: <OrderDetails /> },
+      { path: "/order", element: <Order /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      { index: true, element: <Login /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "forget-password", element: <ForgetPassword /> },
+      { path: "reset-password/:token", element: <ResetPassword /> },
+      { path: "verify-email/:token", element: <VerifyEmail /> },
+      { path: "2fa", element: <TwoFactorAuth /> },
+    ],
+  },
 ]);
 
 const AppRouter = () => <RouterProvider router={router} />;
